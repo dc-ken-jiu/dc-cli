aad9a00e717a0143386ac1d53476a93d9f1e2eae
丁学紧 <accounts_5a503b085f65ba798cb5b115@mail.teambition.com>
2021-02-04 22:32:36 +0800

    refactor:重构脚手架项目,升级到3.0.0版本

---
+++ index.js
@@ -0,0 +1,28 @@
+/*
+ * @Description:
+ * @Author: dingxuejin
+ * @Date: 2021-02-03 18:06:03
+ * @LastEditTime: 2021-02-04 13:47:05
+ * @LastEditors: dingxuejin
+ */
+
+'use strict';
+
+var currentNodeVersion = process.versions.node;
+var semver = currentNodeVersion.split('.');
+var major = semver[0];
+
+if (major < 10) {
+    console.error(
+        'You are running Node ' +
+        currentNodeVersion +
+        '.\n' +
+        'Create React App requires Node 10 or higher. \n' +
+        'Please update your version of Node.'
+    );
+    process.exit(1);
+}
+
+const { init } = require('./create');
+
+init();
